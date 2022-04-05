a441a2282ead5bb226c320609d8c8608a0ad2288
shaybay <pacificconnect310@gmail.com>
2022-04-05 14:16:11 -0700

    part 2.2 done

--- models/flight.js
+++ models/flight.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const destinationSchema = new Schema({
+    airport: {        
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']
+    }, 
+    arrival: Date
+})
 const flightSchema = new Schema({
     Airline: String,
     Airport: String,
     FlightNo: {type: Number, min: 10, max:999}, 
     Departs: Date,
+    destination:[destinationSchema]
 });
 module.exports = mongoose.model('Flight', flightSchema);
 
